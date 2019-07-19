19afe6a161c5a7273c3ced439c707492192f307d
jsapp352 <32817628+jsapp352@users.noreply.github.com>
2019-07-19 00:26:44 -0400

    Removed charset from headers

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -12,7 +12,7 @@ var data = {
 
 const options = {
 	method : 'POST',
-	headers: {"Content-Type": "application/json; charset=UTF-8"},
+	headers: {"Content-Type": "application/json"},
 	body: JSON.stringify(data)
 };
 
